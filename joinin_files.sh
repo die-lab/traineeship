e6cc0c40125a88a8130e83f95d3e03dfdaacc6b4
die-lab <79845154+die-lab@users.noreply.github.com>
2021-08-05 13:20:34 +0200

    Add files via upload

--- joinin_files.sh
+++ joinin_files.sh
@@ -7,13 +7,13 @@ n_pieces=`ls | wc -l`
 echo ""
 echo "working on "$n_pieces" pieces of "${i%_split}
 
-if [ -f ../${i%_split}.fna.prova ]
-then rm -r ../${i%_split}.fna.prova
+if [ -f ../${i%_split}.fna ]
+then rm -r ../${i%_split}.fna
 fi
 
 c=1
 while [ $c -le $n_pieces ] 
-do cat $c"_"$i >> ../${i%_split}.fna.prova 
+do cat $c"_"$i >> ../${i%_split}.fna 
 c=$(( $c +1 ))
 done
 cd .. 
